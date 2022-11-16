13291f774f43427591546be7d29021e5cfa6ba42
Mascherino <code@keedosuul.de>
2022-11-16 16:30:26 +0100

    api: fix linting errors

--- mombot/api.py
+++ mombot/api.py
@@ -1,5 +1,5 @@
 import requests
-import pyourls3 
+import pyourls3
 
 # Annotation imports
 from typing import (
@@ -17,7 +17,7 @@ class RequestException(Exception):
 
     Attributes:
         url --- the requested url
-        message --- explanation of error	
+        message --- explanation of error
     """
 
     def init(self, url, message="Request did not return status code 200"):
@@ -28,9 +28,11 @@ class RequestException(Exception):
 class API():
 
     def __init__(self, yourls_secret: str) -> None:
-        self.yourls: pyourls3.Yourls = pyourls3.Yourls("https://link.keedosuul.de/", key=yourls_secret)
+        url = "https://link.keedosuul.de"
+        self.yourls: pyourls3.Yourls = pyourls3.Yourls(url, key=yourls_secret)
 
-    def get_listings(self, building: str, page_nr: int, amount: int = 1) -> Optional[Dict[Union[str, int], Any]]:
+    def get_listings(self, building: str, page_nr: int, amount: int = 1
+                     ) -> Optional[Dict[Union[str, int], Any]]:
         '''
         Get AtomicHub marketplace listings
 
@@ -42,36 +44,42 @@ class API():
         '''
 
         listings = None
-        r = requests.get(f"https://wax.api.atomicassets.io/atomicmarket/v2/sales?state=1&collection_name=onmars&schema_name=land.plots&mutable_data.{building}={amount}&page={page_nr}&limit=10&order=asc&sort=price")
+        url = f"https://wax.api.atomicassets.io/atomicmarket/v2/" + \
+              f"sales?state=1&collection_name=onmars&schema_name=land.plots" + \
+              f"&mutable_data.{building}={amount}&page={page_nr}&limit=10" + \
+              f"&order=asc&sort=price"
+        r = requests.get(url)
         try:
             if r.status_code != 200:
                 raise RequestException
             else:
                 listings = r.json()['data']
                 if listings:
-                    #listings = extract_data(listings,"sale_id")
+                    # listings = extract_data(listings,"sale_id")
                     links: Dict[Union[str, int], Any] = {}
                     try:
                         for count, item in enumerate(listings):
                             links.setdefault(count, {})
-                            links[count]["link"] = self.yourls.shorten(f"https://wax.atomichub.io/market/sale/{item['sale_id']}")["shorturl"]
-                            links[count]["price"] = int(round(int(item["price"]["amount"]) / wax_precision, 0))
-                            links[count]["token_symbol"] = item["price"]["token_symbol"]
+                            links[count]["link"] = self.yourls.shorten(
+                                f"https://wax.atomichub.io/market/sale/" +
+                                f"{item['sale_id']}")["shorturl"]
+                            links[count]["price"] = int(
+                                round(int(item["price"]["amount"]) /
+                                      wax_precision, 0))
+                            symbol = item["price"]["token_symbol"]
+                            links[count]["token_symbol"] = symbol
                             if len(item["assets"]) > 1:
                                 links[count]["land"] = []
                                 for land in item["assets"]:
                                     try:
-                                        links[count]["land"].append({"rarity": land["data"]["rarity"]})
+                                        links[count]["land"].append(
+                                            {"rarity": land["data"]["rarity"]})
                                     except KeyError:
                                         pass
                             else:
                                 links[count]["land"] = {}
-                                links[count]["land"]["rarity"] = item["assets"][0]["data"]["rarity"]
-                                links[count]["land"]["available_space"] = item["assets"][0]["data"]["available_space"]
-                                # links[count]["land"]["buildings"] = {}
-                                # for building in item["assets"][0]["mutable_data"]:
-                                # 	if not building in ["total_space", "available_space"]:
-                                # 		links[count]["land"]["buildings"][building] = item["assets"][0]["mutable_data"][building]
+                                rarity = item["assets"][0]["data"]["rarity"]
+                                links[count]["land"]["rarity"] = rarity
                         return links
                     except KeyError as e:
                         print(e)
@@ -79,7 +87,6 @@ class API():
                 else:
                     return None
         except KeyError:
-            # raise RequestException("Key data not found in request response text")
             return None
 
     def get_buildings(self) -> Union[Any, None]:
@@ -91,7 +98,9 @@ class API():
         '''
 
         data = None
-        r = requests.get("https://wax.api.atomicassets.io/atomicassets/v1/schemas/onmars/land.plots")
+        url = "https://wax.api.atomicassets.io/atomicassets/v1/schemas/" + \
+              "onmars/land.plots"
+        r = requests.get(url)
         try:
             if r.status_code != 200:
                 raise RequestException
@@ -100,7 +109,6 @@ class API():
                 # print(data)
                 return data
         except KeyError:
-            # raise RequestException("Key data not found in request response text")
             return None
 
     def extract_data(self, json: Dict[str, Any], d: Any) -> List[Any]:
