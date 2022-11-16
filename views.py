dd34c644112cda565f5a745e2718fe6e84213157
Mascherino <code@keedosuul.de>
2022-11-16 15:58:50 +0100

    views: fix linting

--- views.py
+++ views.py
@@ -11,13 +11,15 @@ class Confirm(discord.ui.View):
         self.value = None
 
     @discord.ui.button(label='Confirm', style=discord.ButtonStyle.green)
-    async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button):
+    async def confirm(self, interaction: discord.Interaction,
+                      button: discord.ui.Button) -> None:
         await interaction.response.send_message('Confirming', ephemeral=True)
         self.value = True
         self.stop()
 
     @discord.ui.button(label='Cancel', style=discord.ButtonStyle.grey)
-    async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button):
+    async def cancel(self, interaction: discord.Interaction,
+                     button: discord.ui.Button) -> None:
         await interaction.response.send_message('Cancelling', ephemeral=True)
         self.value = False
         self.stop()
@@ -29,7 +31,8 @@ class Listings(discord.ui.View):
         self.building = building
 
     @discord.ui.button(label="More results", style=discord.ButtonStyle.green)
-    async def more(self, interaction: discord.Interaction, button: discord.ui.Button):
+    async def more(self, interaction: discord.Interaction,
+                   button: discord.ui.Button) -> None:
         config = configparser.ConfigParser()
         config.read("config.cfg")
         api = API(config["yourls"]["secret"])
@@ -37,15 +40,41 @@ class Listings(discord.ui.View):
         self.page += 1
         listings = api.get_listings(self.building, self.page)
         if listings:
-            em_msg = discord.Embed(title="Listings",description=f"Listings containing {self.building} (page {self.page})",color=0x00ff00)
-            em_msg.add_field(name="Listings", value="\n".join([listings[item]["link"] for item in listings.keys()]), inline=True)
-            em_msg.add_field(name="Cost", value="\n".join([str(listings[item]["price"])+ " " + listings[item]["token_symbol"] for item in listings.keys()]), inline=True)
-            em_msg.add_field(name="Land(s)", value="\n".join([listings[item]["land"]["rarity"] if isinstance(listings[item]["land"],dict) 
-                             else "Bundle" for item in listings.keys()]), inline=True)
-            await interaction.followup.send(embed=em_msg, view=Listings(self.building, self.page))
+            em_msg = discord.Embed(
+                title="Listings",
+                description=f"Listings containing {self.building} " +
+                            f"(page {self.page})",
+                color=0x00ff00)
+
+            em_msg.add_field(
+                name="Listings",
+                value="\n".join([
+                    listings[item]["link"] for item in listings.keys()]),
+                inline=True)
+
+            em_msg.add_field(
+                name="Cost",
+                value="\n".join([
+                    str(listings[item]["price"]) + " " +
+                    listings[item]["token_symbol"]
+                    for item in listings.keys()]),
+                inline=True)
+
+            em_msg.add_field(
+                name="Land(s)",
+                value="\n".join([
+                    listings[item]["land"]["rarity"]
+                    if isinstance(listings[item]["land"], dict)
+                    else "Bundle" for item in listings.keys()]),
+                inline=True)
+            listing_view = Listings(self.building, self.page)
+            await interaction.followup.send(embed=em_msg,
+                                            view=listing_view)
         else:
-            em_msg = discord.Embed(title="Listings",color=0x00ff00)
-            em_msg.add_field(name="Listings",value=f"No more listings found. (page {self.page})",inline=False)
+            em_msg = discord.Embed(title="Listings", color=0x00ff00)
+            em_msg.add_field(
+                name="Listings",
+                value=f"No more listings found. (page {self.page})",
+                inline=False)
             await interaction.followup.send(embed=em_msg)
         self.stop()
-        
