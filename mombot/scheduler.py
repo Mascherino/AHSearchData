ffe128ed2f5bd36a41f3ddea42ab056633847e99
Mascherino <code@keedosuul.de>
2022-11-17 02:48:22 +0100

    scheduler: fixed pytz deprecation warning

--- mombot/scheduler.py
+++ mombot/scheduler.py
@@ -1,10 +1,8 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
-from apscheduler.util import undefined
 from apscheduler.job import Job
 
-from discord.ext import commands
-import datetime as dt
+import tzlocal
 
 # Annotation imports
 from typing import (
@@ -18,7 +16,9 @@ class Scheduler(AsyncIOScheduler):
     def __init__(self, mysql_creds: str) -> None:
         url = f"mariadb+pymysql://{mysql_creds}/flask?charset=utf8mb4"
         self.js = {'default': SQLAlchemyJobStore(url=url)}
-        super().__init__(jobstores=self.js)
+        super().__init__(
+            jobstores=self.js,
+            timezone=str(tzlocal.get_localzone()))
 
     def get_user_jobs(self, user: int, string: bool = False
                       ) -> Optional[Sequence[Job]]:
