35c631fa3453de4819cf9da055f022487baf40ca
Mascherino <code@keedosuul.de>
2022-12-15 16:14:26 +0100

    search: added support for mobile layout and * level

--- opportunity/commands/search.py
+++ opportunity/commands/search.py
@@ -7,6 +7,7 @@ from typing import (
     Dict,
     List,
     Literal,
+    Union,
     Any
 )
 
@@ -15,7 +16,7 @@ from discord import app_commands
 from discord.ext import commands
 
 from utils import Color
-from components.views import Listings
+from components.views import Listings, AllLevelListings
 
 if TYPE_CHECKING:
     from opportunity.opportunity import Bot
@@ -24,8 +25,6 @@ async def core_ac(
     interaction: discord.Interaction,
     current: str,
 ) -> List[app_commands.Choice[str]]:
-    print("core_ac")
-    core: Optional[str] = interaction.namespace.core
     choices = [
         'Solar Panel', 'Water Filter', 'C.A.D.', 'Greenhouse',
         'Sab Reactor', 'Smelter', 'Chem Lab', 'Machine Shop',
@@ -33,10 +32,7 @@ async def core_ac(
     ]
     if interaction.namespace.rarity == "Special":
         choices = []
-    elif core:
-        choices = [s for s in choices if core.lower() in s.lower()]
-    else:
-        choices = []
+    choices = sorted(choices)
     return [
         app_commands.Choice(name=building, value=building)
         for building in choices if current.lower() in building.lower()
@@ -51,18 +47,12 @@ async def advanced_ac(
     if ns.rarity == "Special":
         pass
     else:
-        if ns.level == 1:
-            choices.append("Metis Shield")
-        if ns.level <= 5:
-            choices.extend(["Habitat", "Shelter"])
-        if (ns.rarity == "Common" and ns.level <= 10) or \
-                (ns.rarity == "Uncommon" and ns.level <= 8) or \
-                (ns.rarity == "Rare" and ns.level <= 6) or \
-                (ns.rarity in ["Epic", "Legendary", "Mythic"] and
-                 ns.level <= 5):
-            choices.extend(['Rover Works', 'Engineering Bay',
-                            'Thorium Reactor', 'Composter'])
+        choices.append("Metis Shield")
+        choices.extend(["Habitat", "Shelter"])
+        choices.extend(['Rover Works', 'Engineering Bay',
+                        'Thorium Reactor', 'Composter'])
         choices.extend(["Mining Rig", "Polar Workshop", "GrindnBrew"])
+        choices = sorted(choices)
     return [
         app_commands.Choice(name=building, value=building)
         for building in choices if current.lower() in building.lower()
@@ -73,13 +63,12 @@ async def special_ac(
     current: str,
 ) -> List[app_commands.Choice[str]]:
     ns: app_commands.Namespace = interaction.namespace
-    choices = []
+    choices: List[str] = []
     if ns.rarity == "Special":
-        if ns.level <= 5:
-            choices.extend(["Bazaar", "Teashop"])
-        if ns.level <= 6:
-            choices.append("Cantina")
-        choices.extend(['Pirate Radio', 'Library', 'Training Hall', 'Gallery'])
+        choices.extend(["Bazaar", "Teashop"])
+        choices.append("Cantina")
+        choices.extend(['Pirate Radio', 'Library',
+                        'Training Hall', 'Gallery'])
     return [
         app_commands.Choice(name=building, value=building)
         for building in choices if current.lower() in building.lower()
@@ -92,6 +81,7 @@ class Search(commands.Cog):
         self.logger = logging.getLogger("opportunity." + __name__)
         if buildings := self.bot.api.get_buildings():
             self.buildings = {item["name"]: item["name"] for item in buildings}
+        self.maxLevel = self.bot.data["maxLevel"]
 
     @app_commands.command(description="Search for buildings" +
                                       " on plots on AtomicHub")
@@ -101,7 +91,8 @@ class Search(commands.Cog):
             self,
             interaction: discord.Interaction,
             generation: Literal["Gen 1", "Gen 2", "Gen 3"],
-            level: Literal["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+            level: Literal["1", "2", "3", "4", "5", "6", "7",
+                           "8", "9", "10", "*"],
             rarity: Literal["Common", "Uncommon", "Rare", "Epic",
                             "Legendary", "Mythic", "Special"],
             core: str = "",
@@ -109,6 +100,14 @@ class Search(commands.Cog):
             special: str = "",
             amount: int = 1
     ) -> None:
+        if level == "*" and rarity in ["Common", "Uncommon", "Rare"]:
+            em_msg = discord.Embed(
+                title="Error",
+                description="You cannot retrieve all " +
+                            "levels for rarities **Rare** or **lower**",
+                color=Color.RED)
+            await interaction.response.send_message(embed=em_msg)
+            return
         await interaction.response.defer(thinking=True)
         args: Dict[str, str] = {k: v for k, v in locals().copy().items()
                                 if v and k in
@@ -135,6 +134,7 @@ class Search(commands.Cog):
                 description="You must specify exactly one category " +
                             "(core, advanced or special)",
                 color=Color.RED)
+            await interaction.followup.send(embed=em_msg)
             return
         if building in ["thorium reactor", "ground control"]:
             building = building.replace(" ", "-")
@@ -149,15 +149,61 @@ class Search(commands.Cog):
                 building = temp
         building = building + "_" + rarity[0] + str(level)
         self.logger.info(f"Getting listings for {building}")
-        listings = self.bot.api.get_listings(building, 1, amount)
-        if listings:
-            description = f"Listings containing {amount} " + \
-                          f"{list(args.items())[0][1]} (page 1)"
-            em_msg = discord.Embed(
-                title="Listings",
-                description=description,
-                color=Color.GREEN)
-            for i, item in enumerate(listings):
+        building_name: str = building.rsplit("_", 1)[0]
+        if level == "*":
+            level = "1"
+            building = building.replace("*", level)
+            listings = self.bot.api.get_listings(building, 1, amount)
+            lvl: int = int(level)
+            while not listings:
+                if lvl < int(self.maxLevel[building_name][rarity[0]]):
+                    building.replace(level, str(lvl+1))
+                    lvl += 1
+                    listings = self.bot.api.get_listings(building, 1, amount)
+                else:
+                    em_msg = discord.Embed(title="Listings", color=0xff0000)
+                    em_msg.add_field(
+                        name="Listings",
+                        value=f"No listings found. (level {str(lvl)})",
+                        inline=False)
+                    await interaction.followup.send(embed=em_msg)
+                    return
+            view = AllLevelListings(self.bot, building, lvl, 1)
+
+        else:
+            listings = self.bot.api.get_listings(building, 1, amount)
+            view = Listings(building, 1)
+            if not listings:
+                await interaction.followup.send(embed=discord.Embed(
+                    title="Error",
+                    description="Could not find any listings " +
+                                "matching the given parameters",
+                    color=Color.RED))
+                return
+        description = f"Listings containing **{amount}** " + \
+                      f"**{rarity} {list(args.items())[0][1]} " + \
+                      f"Level {level}** (page 1)"
+        em_msg = discord.Embed(
+            title="Listings",
+            description=description,
+            color=Color.GREEN)
+        self._build_embed(interaction, em_msg, listings)
+        await interaction.followup.send(embed=em_msg, view=view)
+
+
+    def _build_embed(
+        self,
+        interaction: discord.Interaction,
+        em_msg: discord.Embed,
+        listings: Dict[Union[str, int], Any]
+    ) -> None:
+        mobile: bool = False
+        if interaction.guild:
+            user = interaction.guild.get_member(interaction.user.id)
+            if isinstance(user, discord.Member):
+                mobile = user.is_on_mobile()
+        if mobile:
+            for item in listings:
                 em_msg.add_field(
                     name=f"{listings[item]['name']}",
                     value="\n".join([
@@ -168,14 +214,27 @@ class Search(commands.Cog):
                         if isinstance(listings[item]["land"], dict)
                         else "Bundle"])
                 )
-            view = Listings(building, 1)
-            await interaction.followup.send(embed=em_msg, view=view)
         else:
-            await interaction.followup.send(embed=discord.Embed(
-                title="Error",
-                description="Could not find any listings " +
-                            "matching the given parameters",
-                color=Color.RED))
+            em_msg.add_field(
+                name="Listings",
+                value="\n".join([
+                    f"[Link]({listings[item]['link']})"
+                    for item in listings.keys()]),
+                inline=True)
+            em_msg.add_field(
+                name="Cost",
+                value="\n".join([
+                    str(listings[item]["price"]) +
+                    " " + listings[item]["token_symbol"]
+                    for item in listings.keys()]),
+                inline=True)
+            em_msg.add_field(
+                name="Land(s)",
+                value="\n".join([
+                    listings[item]["land"]["rarity"]
+                    if isinstance(listings[item]["land"], dict)
+                    else "Bundle" for item in listings.keys()]),
+                inline=True)
 
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(Search(bot))
