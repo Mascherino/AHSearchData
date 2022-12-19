a835a501efe4dfea2056d35ec8db1a6238b19291
Mascherino <code@keedosuul.de>
2022-12-19 01:23:03 +0100

    split_json: fixed logging

--- scripts/split_json.py
+++ scripts/split_json.py
@@ -6,33 +6,28 @@ import json
 import os
 from os.path import dirname as up
 
-from opportunity.utils import setup_logging
-
 def main(args):
-    setup_logging(
-        "split_json",
-        log_path=os.path.join(up(up(__file__)), "logs", "split_json.log"))
     logger = logging.getLogger("split_json")
 
     j = {}
     with open(args.json, "r", encoding="utf-8") as f:
-        logging.info(f"reading {args.json}...")
+        logger.info(f"reading {args.json}...")
         try:
             j = json.load(f)
         except Exception as e:
-            logging.error(e)
+            logger.error(e)
         if args.output and not os.path.isdir(args.output):
-            logging.info(f"dir {args.output} does not exist. Creating...")
+            logger.info(f"dir {args.output} does not exist. Creating...")
             try:
                 os.mkdir(args.output)
             except OSError as e:
-                logging.error(e)
+                logger.error(e)
         for category in j.keys():
             path = os.path.join(args.output, category+".json")
-            logging.info(f"Writing {category+'.json'}, path: {path}...")
+            logger.info(f"Writing {category+'.json'}, path: {path}...")
             with open(path, "w", encoding="utf-8") as x:
                 json.dump(j[category], x)
-            logging.info(f"Successfully written {path}.")
+            logger.info(f"Successfully written {path}.")
 
 
 if __name__ == '__main__':
